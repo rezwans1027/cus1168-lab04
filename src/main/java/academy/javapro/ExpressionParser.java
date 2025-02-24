29375a1ac71d03304668dadf2bfee9c80c911687
rezwans1027 <rezwans1027@gmail.com>
2025-02-23 19:36:50 -0500

    finished homework

--- src/main/java/academy/javapro/ExpressionParser.java
+++ src/main/java/academy/javapro/ExpressionParser.java
@@ -11,45 +11,56 @@ class ExpressionParser {
 
     // expr → expr + term
     public double parseExpression() {
-        // TODO: Get the leftmost term value by calling parseTerm()
-        // TODO: While we haven't reached the end of input and current char is '+'
-        // TODO: Increment position to skip the '+' operator
-        // TODO: Get the next term on the right
-        // TODO: Add the right term to our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseExpression");
+        double result = parseTerm();
+        while (position < input.length() && input.charAt(position) == '+') {
+            position++;
+            double rightTerm = parseTerm();
+            result += rightTerm;
+        }
+        return result;
     }
 
     // term → term * factor
     private double parseTerm() {
-        // TODO: Get the leftmost factor value by calling parseFactor()
-        // TODO: While we haven't reached the end of input and current char is '*'
-        // TODO: Increment position to skip the '*' operator
-        // TODO: Get the next factor on the right
-        // TODO: Multiply the right factor with our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseTerm");
+        double result = parseFactor();
+        while (position < input.length() && input.charAt(position) == '*') {
+            position++;
+            double rightFactor = parseFactor();
+            result *= rightFactor;
+        }
+        return result;
     }
 
     // factor → ( expr )
     private double parseFactor() {
-        // TODO: Check if we have an opening parenthesis and haven't reached end of input
-        // TODO: Increment position to skip the opening parenthesis
-        // TODO: Parse the expression inside the parentheses
-        // TODO: Increment position to skip the closing parenthesis
-        // TODO: Return the result of the parenthesized expression
-        // TODO: If no parentheses, parse and return a number
-        throw new UnsupportedOperationException("Implement parseFactor");
+        if (position < input.length() && input.charAt(position) == '(') {
+            position++;
+            double value = parseExpression();
+            position++;
+            return value;
+        } else {
+            return parseNumber();
+        }
     }
 
     // Parse a numeric value
     private double parseNumber() {
-        // TODO: Create a StringBuilder to collect digits
-        // TODO: While we haven't reached the end and current char is digit or decimal point
-        // TODO: Append the current digit to our number string
-        // TODO: Move to next character
-        // TODO: Convert the string of digits to a double and return it
-        throw new UnsupportedOperationException("Implement parseNumber");
+        StringBuilder sb = new StringBuilder();
+        boolean hasDecimalPoint = false;
+        while (position < input.length()) {
+            char c = input.charAt(position);
+            if (Character.isDigit(c)) {
+                sb.append(c);
+                position++;
+            } else if (c == '.' && !hasDecimalPoint) {
+                sb.append(c);
+                hasDecimalPoint = true;
+                position++;
+            } else {
+                break;
+            }
+        }
+        return Double.parseDouble(sb.toString());
     }
 
     public static void main(String[] args) {
@@ -62,7 +73,6 @@ class ExpressionParser {
                 "1.5 + 2.5 * 3"       // Decimal numbers
         };
 
-        // Process each test case
         for (String expression : testCases) {
             System.out.println("\nTest Case: " + expression);
             try {
